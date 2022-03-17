59a73c017c9cefc25bcfc42ba5edb7eb1db7b94a
‘Gelila’ <79760415+Gelila21@users.noreply.github.com>
2022-03-17 15:28:49 -0700

    working on resizableArray Stack

--- ResizeableArrayStack.java
+++ ResizeableArrayStack.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
 public final class ResizeableArrayStack<T> implements StackInterface {
     private T[] stack;
     private int topIndex;
@@ -11,16 +14,117 @@ public final class ResizeableArrayStack<T> implements StackInterface {
 
     public ResizeableArrayStack(int initialCapacity)
     {
-        integrityOK = false;
+        IntegrityOK = false;
         checkCapacity(initialCapacity);
 
-        @SuppressWarnings("unchecked"
-        )
+        @SuppressWarnings("unchecked" )
+        T[] tempStack = (T[])new Object[initialCapacity];
+       stack = tempStack;
+topIndex = -1;
+       IntegrityOK = true;
+    }
+    private void ensureCapacity()
+{
+   if (topIndex >= stack.length - 1) // If array is full, double its size
+   {
+      int newLength = 2 * stack.length;
+      checkCapacity(newLength);
+      stack = Arrays.copyOf(stack, newLength);
+   } // end if
+} 
+    @Override
+    public void push(Object newEntry) {
+       // checkInyegrity();
+   ensureCapacity();
+   stack[topIndex + 1] = (T) newEntry;
+   topIndex++;
+        
     }
 
-    public void push(T newEntry) {
-        checkIntegrity();
-        ensureCapacity();
+  
 
+    @Override
+    public Object pop() {
+        // TODO Auto-generated method stub
+        return null;
     }
+
+    @Override
+    public Object peek() {
+       // checkIntegrity();
+        if (isEmpty())
+           throw new EmptyStackException();
+        else
+           return stack[topIndex];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return topIndex < 0;
+    }
+
+    @Override
+    public void clear() {
+       // checkIntegrity();
+      
+      // Remove references to the objects in the stack,
+      // but do not deallocate the array
+      while (topIndex > -1)
+      {
+          stack[topIndex] = null;
+          topIndex--;
+      }
+        
+    }
+    private static boolean isOperator(char i) {
+        return precedence(i) > 0;
+    }
+    private static int precedence(char i) {
+
+        if (i == '(' || i == ')') return 1;
+        else if (i == '-' || i == '+') return 2;
+        else if (i == '*' || i == '/') return 3;
+        else return 0;
+    }
+
+    @Override
+    public String convertToPostfix(String infix) {
+        String postfix = "";
+        ResizeableArrayStack<Character> operator = new ResizeableArrayStack<Character>();
+        
+char popped;
+
+        for (int i = 0; i < infix.length(); i++) {
+
+            char get = infix.charAt(i);
+
+            if (!isOperator(get))
+                postfix += get;
+
+            else if (get == ')')
+                while ((popped = (char) operator.pop()) != '(')
+                    postfix += popped;
+
+            else {
+                while (!operator.isEmpty() && get != '(' && precedence((char) operator.peek()) >= precedence(get))
+                    postfix += operator.pop();
+
+                operator.push(get);
+            }
+        }
+        // pop any remaining operator
+        while (!operator.isEmpty())
+            postfix += operator.pop();
+
+        return postfix;
+        
+    }
+
+
+    @Override
+    public String evaluatePostfix(String postfix) {
+       
+    }
+
+   
 }
