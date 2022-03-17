f6fa9be56577d42c69c4a5eb39ebe20924844a2a
uhnguyen101 <70361768+uhnguyen101@users.noreply.github.com>
2022-03-16 17:14:02 -0700

    added template to resizable array stack

--- ResizeableArrayStack.java
+++ ResizeableArrayStack.java
@@ -1,1 +1,26 @@
+public final class ResizeableArrayStack<T> implements StackInterface {
+    private T[] stack;
+    private int topIndex;
+    private boolean IntegrityOK = false;
+    private static final int DEFAULT_CAPACITY = 50;
+    private static final int MAX_CAPACITY = 10000;
 
+    public ResizeableArrayStack() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    public ResizeableArrayStack(int initialCapacity)
+    {
+        integrityOK = false;
+        checkCapacity(initialCapacity);
+
+        @SuppressWarnings("unchecked"
+        )
+    }
+
+    public void push(T newEntry) {
+        checkIntegrity();
+        ensureCapacity();
+
+    }
+}
