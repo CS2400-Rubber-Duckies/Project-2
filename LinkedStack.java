90504aa519e71ee6f698a37791aed51ed6ea4c4a
uhnguyen101 <70361768+uhnguyen101@users.noreply.github.com>
2022-03-19 16:23:21 -0700

    added node

--- LinkedStack.java
+++ LinkedStack.java
@@ -35,8 +35,32 @@ public class LinkedStack<T> implements StackInterface<T> {
         topNode = null;
     }
 
+    private class Node {
+        T data;
+        Node next;
 
-        
+        public Node(T val, Node n) {
+            data = val;
+            next = n;
+        }
+
+        public void setData(T val) {
+            data = val;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node n) {
+            next = n;
+        }
+    }
+
     public String convertToPostfix() {
         // TODO Auto-generated method stub
         return null;
