4c4d26322b3deecce8a33a9e8f384ec9d12cdc98
uhnguyen101 <70361768+uhnguyen101@users.noreply.github.com>
2022-03-19 21:55:56 -0700

    wokring on linkedstack

--- LinkedStack.java
+++ LinkedStack.java
@@ -86,33 +86,48 @@ public class LinkedStack<T> implements StackInterface<T> {
 
 }
 
-// Algorithm convertToPostfix(infix)
-// // Converts an infix expression to an equivalent postfix expression.
-// operatorStack = a new empty stack
-// postfix = a new empty string
-// while (infix has characters left to parse)
-// {
-// nextCharacter = next nonblank character of infix switch (nextCharacter)
-// {
-// case variable:
-// Append nextCharacter to postfix break
-// case '^' : operatorStack.push(nextCharacter) break
-// case '+' : case '-' : case '*' : case '/' : while (!operatorStack.isEmpty()
-// and
-// precedence of nextCharacter <= precedence of operatorStack.peek())
-// {
-// Append operatorStack.peek() to postfix operatorStack.pop()
-// } operatorStack.push(nextCharacter) break
-// case '( ' : operatorStack.push(nextCharacter) break
-// case ')' : // Stack is not empty if infix expression is valid topOperator =
-// operatorStack.pop()
-// while (topOperator != '(')
-// {
-// Append topOperator to postfix
-// topOperator = operatorStack.pop() }
-// break
-// default: break // Ignore unexpected characters
-// while (!operatorStack.isEmpty()) {
-// topOperator = operatorStack.pop()
-// Append topOperator to postfix }
-// return postfix
+    // Algorithm convertToPostfix(infix)
+    // // Converts an infix expression to an equivalent postfix expression.
+    // operatorStack = a new empty stack
+    // postfix = a new empty string
+    // while (infix has characters left to parse)
+    // {
+    // nextCharacter = next nonblank character of infix switch (nextCharacter)
+    // {
+    // case variable:
+    // Append nextCharacter to postfix break
+    // case '^' : operatorStack.push(nextCharacter) break
+    // case '+' : case '-' : case '*' : case '/' : while (!operatorStack.isEmpty()
+    // and
+    // precedence of nextCharacter <= precedence of operatorStack.peek())
+    // {
+    // Append operatorStack.peek() to postfix operatorStack.pop()
+    // } operatorStack.push(nextCharacter) break
+    // case '( ' : operatorStack.push(nextCharacter) break
+    // case ')' : // Stack is not empty if infix expression is valid topOperator =
+    // operatorStack.pop()
+    // while (topOperator != '(')
+    // {
+    // Append topOperator to postfix
+    // topOperator = operatorStack.pop() }
+    // break
+    // default: break // Ignore unexpected characters
+    // while (!operatorStack.isEmpty()) {
+    // topOperator = operatorStack.pop()
+    // Append topOperator to postfix }
+    // return postfix
+
+    public String convertToPostfix(String infix) {
+        LinkedStack<T> operatorStack = new LinkedStack<T>();
+        String postfix = "";
+        return postfix;
+        for (int i = 0; i < infix.length(); i++) {
+            char character = infix.charAt(i);
+
+            if (!isOperator(character))
+
+        }
+        return postfix;
+
+    }
+}
